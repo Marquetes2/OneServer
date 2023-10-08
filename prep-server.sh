51bfa23abddfca6601e2b00efe32d91032bd6f97
Marquetes2 <marquetes2@gmail.com>
2023-10-08 15:32:06 +0200

    Adapted script to Arch Linux

--- prep-server.sh
+++ prep-server.sh
@@ -9,7 +9,7 @@ echo "____________________________________________"
 echo "         Docker and Docker Compose          "
 echo "--------------------------------------------"
 # Install Docker and Docker Compose
-sudo pamac install --no-confirm docker docker-compose
+sudo pacman -S --noconfirm docker docker-compose
 
 # Create non-root user for docker, with privileges (not docker rootless)
 sudo groupadd docker
@@ -27,14 +27,14 @@ sudo sh -c "echo export COMPOSE_FILE="/home/${USER}/docker/compose.yml" >> /etc/
 echo "            Wireguard VPN Tools             "
 echo "--------------------------------------------"
 # If you used the post-install script, this should already be installed
-sudo pamac install --no-confirm wireguard-tools
+sudo pacman -S --noconfirm wireguard-tools
 
 echo "                   BTRBK                    "
 echo "--------------------------------------------"
 echo "Swiss handknife-like tool to automate snapshots & backups of personal data" 
 # available in the Arch User Repository (AUR) thus installed via Pamac. Will be automatically updated just like official repository packages. 
-sudo pamac install --no-confirm btrbk
-sudo pamac install --no-confirm mbuffer
+sudo pacman -S --noconfirm btrbk
+sudo pacman -S --noconfirm mbuffer
 
 echo "        RUN-IF-TODAY & ENABLE CRON          "
 echo "--------------------------------------------"
@@ -48,36 +48,32 @@ echo "                   NOCACHE                  "
 echo "--------------------------------------------"
 echo "handy when moving lots of files at once in the background, without filling up cache and slowing down the system."
 # available in the Arch User Repository (AUR) thus installed via Pamac. Will be automatically updated just like official repository packages. 
-sudo pamac install --no-confirm nocache
+sudo pacman -S --noconfirm nocache
 
 echo "                    GRSYNC                  "
 echo "--------------------------------------------"
-echo "Friendly UI for rsync"
-sudo pamac install --no-confirm grsync
+echo "Friendly UI for rsync. Not installed"
 
 echo "                  LM_SENSORS                "
 echo "--------------------------------------------"
 echo "to be able to read out all sensors" 
-sudo pamac install --no-confirm lm_sensors
+sudo pacman -S --noconfirm lm_sensors
 sudo sensors-detect --auto
 
 echo "          S.M.A.R.T. monitoring             "
 echo "--------------------------------------------"
-echo "to be able to read SMART values of drives" 
-sudo pamac install --no-confirm smartmontools
-sudo sed -i -e "s^#DEVICESCAN -a^DEVICESCAN -a -o on -S on -n standby,q -s (S/../.././02|L/../../6/03) -W 1,35,60 -m default^g" /etc/smartd.conf
-sudo systemctl enable smartd
+echo "to be able to read SMART values of drives. Not intalled" 
 
 echo "                 HD PARM                    "
 echo "--------------------------------------------"
 echo "to be able to configure drive parameters" 
-sudo pamac install --no-confirm hdparm
+sudo pacman -S --noconfirm hdparm
 
 echo "                 MERGERFS                  "
 echo "-------------------------------------------"
 echo "pool drives to make them appear as 1 without raid"
 # available in the Arch User Repository (AUR) thus installed via Pamac. Will be automatically updated just like official repository packages. 
-sudo pamac install --no-confirm mergerfs
+sudo pacman -S --noconfirm mergerfs
 
 
 echo "______________________________________________________"
@@ -112,27 +108,11 @@ echo "------------------------------"
 # To run the FileRun commands as the regular user, we add an env variable for that user to the only env that is accessible by root cronjobs: 
 sudo sh -c "echo LOGUSER=${USER} >> /etc/environment"
 
-echo " Add useful items to App Menu "
-echo "------------------------------"
-gsettings set org.gnome.shell.extensions.arcmenu pinned-app-list "['ONLYOFFICE Desktop Editors', '', 'org.onlyoffice.desktopeditors.desktop', 'LibreOffice Writer', '', 'libreoffice-writer.desktop', 'LibreOffice Calc', '', 'libreoffice-calc.desktop', 'LibreOffice Impress', '', 'libreoffice-impress.desktop', 'Document Scanner', '', 'simple-scan.desktop', 'Text Editor', '', 'pluma.desktop', 'Calculator', '', 'org.gnome.Calculator.desktop', 'digiKam', '', 'org.kde.digikam.desktop', 'Pinta Image Editor', '', 'pinta.desktop', 'GNU Image Manipulation Program', '', 'gimp.desktop', 'Strawberry', '', 'org.strawberrymusicplayer.strawberry.desktop', 'Audacity', '', 'audacity.desktop', 'LosslessCut', '', 'losslesscut-bin.desktop', 'HandBrake', '', 'fr.handbrake.ghb.desktop', 'BleachBit', '', 'org.bleachbit.BleachBit.desktop', 'Tweaks', '', 'org.gnome.tweaks.desktop', 'Extension Manager', '', 'com.mattjakeman.ExtensionManager.desktop', 'Add/Remove Software', '', 'org.manjaro.pamac.manager.desktop', 'System Monitor', '', 'gnome-system-monitor.desktop', 'Disks', '', 'org.gnome.DiskUtility.desktop']"
+echo " Add useful items to App Menu (Ignored)"
+echo "---------------------------------------"
 
-echo "  Add filemanager bookmarks   "
-echo "------------------------------"
-# Add CLI to Panel Favourites
-gsettings set org.gnome.shell favorite-apps "['nemo.desktop', 'org.gnome.Terminal.desktop', 'firefox.desktop', 'org.gnome.gThumb.desktop', 'pluma.desktop', 'org.gnome.Calculator.desktop']"
-
-# Set Nemo bookmarks, reflecting folder that will be renamed later (Videos>Media)
-truncate -s 0 $HOME/.config/gtk-3.0/bookmarks
-tee -a $HOME/.config/gtk-3.0/bookmarks &>/dev/null << EOF
-file:///home/${USER}/docker Docker
-file:///mnt/drives Drives
-file:///mnt/pool Pool
-file:///home/${USER}/Downloads Downloads
-file:///home/${USER}/Documents Documents
-file:///home/${USER}/Music Music
-file:///home/${USER}/Pictures Pictures
-file:///home/${USER}/Media Media
-EOF
+echo "  Add filemanager bookmarks (Ignored)"
+echo "-------------------------------------"
 
 echo "  Optimise power consumption  "
 echo "------------------------------"
@@ -211,8 +191,8 @@ systemctl enable --now wgui.{path,service}
 echo "    EMAIL NOTIFICATIONS       "
 echo "------------------------------"
 # allow system to send email notifications - Configure smtp according to Arch wiki
-sudo pamac install --no-confirm msmtp
-sudo pamac install --no-confirm s-nail
+sudo pacman -S --noconfirm msmtp
+sudo pacman -S --noconfirm s-nail
 # link sendmail to msmtp
 sudo ln -s /usr/bin/msmtp /usr/bin/sendmail
 sudo ln -s /usr/bin/msmtp /usr/sbin/sendmail
@@ -273,25 +253,8 @@ case ${answer:0:1} in
 esac
 
 
-echo "  on-demand btrfs root mount  "
-echo "-------------------------------"
-# on-demand systemdrive mountpoint 
-## The MANJARO GNOME POST INSTALL SCRIPT has created a mountpoint for systemdrive. If that script was not used, create the mountpoint now:
-# Get device path of systemdrive, for example "/dev/nvme0n1p2" via #SYSTEMDRIVE=$(df / | grep / | cut -d" " -f1)
-if sudo grep -Fq "/mnt/drives/system" /etc/fstab; then echo already added by post-install script; 
-else 
-# Add an ON-DEMAND mountpoint in FSTAB for the systemdrive, to easily do a manual mount when needed (via "sudo mount /mnt/drives/system")
-sudo mkdir -p /mnt/drives/system
-# Get the systemdrive UUID
-fs_uuid=$(findmnt / -o UUID -n)
-# Add mountpoint to FSTAB
-sudo tee -a /etc/fstab &>/dev/null << EOF
-
-# Allow easy manual mounting of btrfs root subvolume                         
-UUID=${fs_uuid} /mnt/drives/system  btrfs   subvolid=5,defaults,noatime,noauto  0  0
-EOF
-fi
-sudo mount -a
+echo "  on-demand btrfs root mount (Ignored)"
+echo "--------------------------------------"
 
 echo "        Docker subvolume       "
 echo "-------------------------------"
@@ -353,70 +316,8 @@ echo "______________________________________________________"
 echo "           OPTIONAL TOOLS OR CONFIGURATIONS           "
 echo "______________________________________________________"
 echo "--------------------------------------------------------------------------------------------------------------"
-echo "Download recommended/best-practices configuration for QBittorrent: to download media, torrents? (recommended)" 
-read -p "y or n ?" answer
-case ${answer:0:1} in
-    y|Y )
-        sudo mkdir -p $HOME/docker/qbittorrent/config
-        sudo chown ${USER}:${USER} $HOME/docker/qbittorrent/config
-        wget -O $HOME/docker/qbittorrent/config/qBittorrent.conf https://raw.githubusercontent.com/zilexa/Homeserver/master/docker/qbittorrent/config/qBittorrent.conf
-        sudo chmod 644 $HOME/docker/qbittorrent/config/qBittorrent.conf
-    ;;
-    * )
-        echo "SKIPPED downloading QBittorrent config file.."
-    ;;
-esac
 
-
-echo "--------------------------------------------------------------------------------------------------------------"
-echo "Get the PIA VPN script to auto-update Qbittorrent portforwarding? (recommended if you will use PIA VPN for downloads)" 
-read -p "y or n ?" answer
-case ${answer:0:1} in
-    y|Y )
-        echo " PIA VPN script to auto-update Qbittorrent  "
-        echo "--------------------------------------------"
-        mkdir -p $HOME/docker/vpn-proxy/pia-shared
-        wget -O $HOME/docker/vpn-proxy/pia-shared/updateport-qb.sh https://raw.githubusercontent.com/zilexa/Homeserver/master/docker/vpn-proxy/pia-shared/updateport-qb.sh
-        chmod +x $HOME/docker/vpn-proxy/pia-shared/updateport-qb.sh
-        echo "DONE! Don't forget to enter your QBittorrent credentials in the script after you have changed them in the webUI"
-        echo "(default is admin/adminadmin)."
-    ;;
-    * )
-        echo "SKIPPED getting PIA VPN script for auto-updating QB portforwarding.."
-    ;;
-esac
-
-
-echo "Install SNAPRAID-BTRFS for parity-based backups? (recommended if you will pool drives via MergerFS instead of BTRFS RAID)"
-read -p "y or n ?" answer
-case ${answer:0:1} in
-    y|Y )
-        echo "Installing required tools: snapraid, Snapraid-btrfs, snapraid-btrfs-runner mailscript and snapper.."
-        sudo pamac install --no-confirm snapraid 
-        sudo pamac install --no-confirm snapraid-btrfs-git
-        # Install snapraid-btrfs-runner
-        wget -O $HOME/docker/HOST/snapraid/master.zip https://github.com/fmoledina/snapraid-btrfs-runner/archive/refs/heads/master.zip
-        unzip $HOME/docker/HOST/snapraid/master.zip
-        mv $HOME/docker/HOST/snapraid/snapraid-btrfs-runner-master $HOME/docker/HOST/snapraid/snapraid-btrfs-runner
-        rm $HOME/docker/HOST/snapraid/master.zip
-        # Install snapper, required for snapraid-btrfs 
-        sudo pamac install --no-confirm snapper-gui
-        # Get snapper default template
-        sudo wget -O /etc/snapper/config-templates/default https://raw.githubusercontent.com/zilexa/Homeserver/master/docker/HOST/snapraid/snapper/default
-        # get SnapRAID config
-        sudo wget -O $HOME/docker/HOST/snapraid/snapraid.conf https://raw.githubusercontent.com/zilexa/Homeserver/master/docker/HOST/snapraid/snapraid.conf
-        sudo ln -s $HOME/docker/HOST/snapraid/snapraid.conf /etc/snapraid.conf
-        # DONE !
-        # MANUALLY: Create a root subvolume on your fastest drives named .snapraid, this wil contain snapraid content file. 
-        # MANUALLY: customise the $HOME/docker/HOST/snapraid/snapraid.conf file to your needs. 
-        # MANUALLY: follow instructions in the guide 
-        # Get drive IDs
-        #ls -la /dev/disk/by-id/ | grep part1  | cut -d " " -f 11-20
-    ;;
-    * )
-        echo "Skipping Snapraid, Snapraid-BTRFS, snapraid-btrfs-runner and snapper"
-    ;;
-esac
+echo "Ignored all"
 
 echo "                                                                               "        
 echo "==============================================================================="
